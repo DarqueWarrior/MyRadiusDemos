9a1cb8bfccc94ab55d47d0e32b2841ec053ec60f
Donovan Brown <dlb@dlbonline.com>
2021-10-06 15:49:27 +0000

    Init

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+ 
+echo "on-create start" >> ~/status
+
+# log into azure cli
+az login --service-principal -t $TENANT -u $APPID -p $PASSWORD
+
+# set the subscription
+az account set -s $SUBID
+
+# set defaults
+az config set core.output=table
+
+# install PowerShell modules
+pwsh -Command "& {Install-Module -Name Trackyon.Utils, VSTeam, powershell-yaml -Force}"
+ 
+echo "on-create complete" >> ~/status
