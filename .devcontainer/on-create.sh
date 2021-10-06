4f3ee440c81ee8baf2d36e496088296e337f1d12
Donovan Brown <dlb@dlbonline.com>
2021-10-06 16:32:22 +0000

    Moved to sh

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,6 +2,9 @@
  
 echo "on-create start" >> ~/status
 
+# download and install the Radius vs code extension 
+wget https://get.radapp.dev/tools/vscode/stable/rad-vscode-bicep.vsix && code --install-extension rad-vscode-bicep.vsix
+
 # log into azure cli
 az login --service-principal -t $TENANT -u $APPID -p $PASSWORD
 
