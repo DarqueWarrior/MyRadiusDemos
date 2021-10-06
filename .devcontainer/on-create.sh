cb313d73537cd80d6e0a4e3cf2e52de3baf6fa51
Donovan Brown <dlb@dlbonline.com>
2021-10-06 17:48:47 +0000

    delete ext file.

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -3,7 +3,9 @@
 echo "on-create start" >> ~/status
 
 # download and install the Radius vs code extension 
-wget https://get.radapp.dev/tools/vscode/stable/rad-vscode-bicep.vsix && code --install-extension rad-vscode-bicep.vsix && rm rad-vscode-bicep.vsix
+wget https://get.radapp.dev/tools/vscode/stable/rad-vscode-bicep.vsix
+code --install-extension rad-vscode-bicep.vsix
+rm rad-vscode-bicep.vsix
 
 # log into azure cli
 az login --service-principal -t $TENANT -u $APPID -p $PASSWORD
